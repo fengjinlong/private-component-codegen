3ba4921373a1733c86a69661c48ab7f6a21a93a1
IamLiuLv <liulv_915@163.com>
2024-10-26 20:28:44 +0800

    feat: TodoList biz component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="flex flex-col items-center justify-center h-screen bg-red-500">
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default Home;
